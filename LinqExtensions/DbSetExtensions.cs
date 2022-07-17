6687378e0e27786204acf391480709df2738d71c
Sealkeen <lettersforlotro@gmail.com>
2022-07-17 17:37:01 +0300

    Added CreateListAsync extension method

---
+++ LinqExtensions/DbSetExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExtensions
+{
+    public static class DbSetExtensions
+    {
+        public static Task<List<T>> CreateListAsync<T>(this IQueryable<T> query)
+        {
+            var tsk = Task.Factory.StartNew(new Func<List<T>>(delegate 
+            {
+                return query.ToList();
+            }));
+
+            return tsk;
+        }
+    }
+}
